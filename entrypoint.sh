49c0c35fad85545a5fff5f79f5a7509b6a8346d1
Mayank Gupta <mkgupta74d@gmail.com>
2021-05-05 20:14:11 +0200

    updated and bug fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -51,10 +51,6 @@ fi # [ -f $5 ]
 
 chown -R --reference=. $FILE_DIR
 
-apt-get install -y file
-
-
-
 
 FILES_COUNT=`ls $FILE_DIR | wc -l`
 
@@ -74,7 +70,7 @@ if [ $FILES_COUNT = 1 ]
 then
     echo "::set-output name=location::$WORKDIR/$FILE_DIR/$RENAME"
     echo "::set-output name=filename::$RENAME"
-    echo "::set-output name=content_type::$(ls $FILE_DIR | file --mime-type $FILE_DIR/$(< /dev/stdin) | awk '//{ print $2 }')"
+    echo "::set-output name=content_type::$(file --mime-type $FILE_DIR/$RENAME | awk '//{ print $2 }')"
 else
     echo "::set-output name=location::$WORKDIR/$FILE_DIR"
     echo "::set-output name=filename::NULL"
